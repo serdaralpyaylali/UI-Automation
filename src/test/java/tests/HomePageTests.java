e8e22ff48ebaa2ca6b0587a87c43efe34315e787
alp.yaylali <alp.yaylali.adm@diyanet.gov.tr>
2026-02-07 23:44:35 +0300

    Tesct Case 1 is completed

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -2,37 +2,28 @@ package tests;
 
 
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.HomePage;
 import pages.AboutPage;
 import utilities.Driver;
 
 public class HomePageTests {
     private HomePage homePage;
-    private AboutPage aboutPage;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
-        Driver.getWebDriver().get("https://en.wikipedia.org/");
+        Driver.getWebDriver().get("https://www.wikipedia.org/");
         homePage = new HomePage(Driver.getWebDriver());
-        aboutPage = new AboutPage(Driver.getWebDriver());
     }
 
-    @Test
-    public void checkTitle() {
-        boolean checkTitle = homePage.isTitleCorrect("Wikipedia");
-        Assert.assertTrue(checkTitle, "Title is incorrect");
+    @Test(groups = {"smoke", "ui"})
+    public void TC_UI_001_HomePageLoadsSuccessfully() {
+        Assert.assertTrue(homePage.isTitleCorrect("Wikipedia"), "Title is incorrect");
+        Assert.assertTrue(homePage.isLanguageButtonVisible(), "Language button is not displayed");
+        Assert.assertTrue(homePage.isSearchBarVisible(), "Search bar is not displayed");
     }
 
-//    @Test
-//    public void loginSuccesfully(){
-//        homePage.login("tomsmith", "SuperSecretPassword!");
-//        Assert.assertTrue(aboutPage.isLoginSuccesful(),"Login is unsuccesful");
-//    }
-
-    @AfterTest
+    @AfterMethod
     public void teardown(){
         Driver.closeWebDriver();
     }
