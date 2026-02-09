eafc84bd8a2ff7d310d706b1b469c6a249d6d280
Alp Yaylalı <serdaralpyaylali@gmail.com>
2026-02-09 22:28:51 +0300

    Test case 3 and 4 is added

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -5,18 +5,21 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import pages.ArticlePage;
 import pages.HomePage;
-import pages.AboutPage;
+import pages.SearchResultsPage;
 import utilities.Driver;
 
 public class HomePageTests {
     private HomePage homePage;
     private ArticlePage articlePage;
+    private SearchResultsPage searchResultsPage;
 
     @BeforeMethod
     public void setUp() {
         Driver.getWebDriver().get("https://www.wikipedia.org/");
         homePage = new HomePage(Driver.getWebDriver());
         articlePage = new ArticlePage(Driver.getWebDriver());
+        searchResultsPage = new SearchResultsPage(Driver.getWebDriver());
+
     }
 
     @Test(groups = {"smoke", "ui"})
@@ -35,6 +38,28 @@ public class HomePageTests {
         Assert.assertTrue(articlePage.getHeading().toLowerCase().contains("Test automation".toLowerCase()),
                 "Heading is incorrect");
     }
+
+    @Test(groups = {"smoke", "ui"})
+    public void TC_UI_003_WordDoesNotAppearWithSearch() {
+        homePage.selectLanguage("en");
+        homePage.searchAndClick("asd123");
+        Assert.assertTrue(searchResultsPage.getResultText().contains("page does not exist"),
+                "Article page did not appear");
+        Assert.assertFalse(
+                Driver.getWebDriver().getTitle().toLowerCase().contains("error"),
+                "Page crashed"
+        );
+    }
+    @Test(groups = {"smoke", "ui"})
+    public void TC_UI_004_ChangeLanguageOfWebsite() {
+        homePage.selectLanguage("en");
+        homePage.searchAndClick("asd123");
+        Assert.assertTrue(Driver.getWebDriver().getCurrentUrl().contains("en.wikipedia.org"));
+        Assert.assertTrue(articlePage.getHeading().toLowerCase().contains("Search".toLowerCase()),
+                "Heading is incorrect");
+    }
+
+
     @AfterMethod
     public void teardown(){
         Driver.closeWebDriver();
